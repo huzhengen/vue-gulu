fd7ad5fe3338bbde3e2b8cf1ad6b1c56ee7a4c21
卢方 <huzhengen@163.com>
2019-02-12 18:05:38 +0800

    button的iconPosition

--- src/button.vue
+++ src/button.vue
@@ -11,7 +11,18 @@
 
 <script>
     export default{
-        props: ['icon', 'iconPosition']
+//        props: ['icon', 'iconPosition'],
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(value){
+//                    return !(value !== 'left' && value !== 'right');
+                    return value === 'left' || value === 'right'
+                },
+            }
+        }
     }
 </script>
 
