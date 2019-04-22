72ea56e356ded4562aff8cfb66032ec83fe65395
卢方 <huzhengen@163.com>
2019-04-22 16:35:47 +0800

    自动化测试

--- src/app.js
+++ src/app.js
@@ -16,91 +16,101 @@ new Vue({
     },
 })
 
-// 单元测试
 import chai from 'chai'
 import spies from 'chai-spies'
 chai.use(spies)
 const expect = chai.expect
 
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData:{
-            icon: 'settings',
-        }
-    })
-    vm.$mount()
-    let useElement = vm.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-settings')
-    vm.$el.remove()
-    vm.$destroy()
-}
+try {
+    // 单元测试
 
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData:{
-            icon: 'settings',
-            loading: true,
-        }
-    })
-    vm.$mount()
-    let useElement = vm.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-loading')
-    vm.$el.remove()
-    vm.$destroy()
-}
+    {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+            }
+        })
+        vm.$mount()
+        let useElement = vm.$el.querySelector('use')
+        let href = useElement.getAttribute('xlink:href')
+        expect(href).to.eq('#i-settings')
+        vm.$el.remove()
+        vm.$destroy()
+    }
 
-{
-    const div = document.createElement('div')
-    document.getElementById('app').appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData:{
-            icon: 'settings',
-        }
-    })
-    vm.$mount(div)
-    let svg = vm.$el.querySelector('svg')
-    let {order} = window.getComputedStyle(svg)
-    expect(order).to.eq('1')
-    vm.$el.remove()
-    vm.$destroy()
-}
+    {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+                loading: true,
+            }
+        })
+        vm.$mount()
+        let useElement = vm.$el.querySelector('use')
+        let href = useElement.getAttribute('xlink:href')
+        expect(href).to.eq('#i-loading')
+        vm.$el.remove()
+        vm.$destroy()
+    }
 
-{
-    const div = document.createElement('div')
-    document.getElementById('app').appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData:{
-            icon: 'settings',
-            iconPosition: 'right',
-        }
-    })
-    vm.$mount(div)
-    let svg = vm.$el.querySelector('svg')
-    let {order} = window.getComputedStyle(svg)
-    expect(order).to.eq('2')
-    vm.$el.remove()
-    vm.$destroy()
-}
+    {
+        const div = document.createElement('div')
+        document.getElementById('app').appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+            }
+        })
+        vm.$mount(div)
+        let svg = vm.$el.querySelector('svg')
+        let { order } = window.getComputedStyle(svg)
+        expect(order).to.eq('1')
+        vm.$el.remove()
+        vm.$destroy()
+    }
 
-{
-    // mock
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData:{
-            icon: 'settings',
-            iconPosition: 'right',
-        }
+    {
+        const div = document.createElement('div')
+        document.getElementById('app').appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+                iconPosition: 'right',
+            }
+        })
+        vm.$mount(div)
+        let svg = vm.$el.querySelector('svg')
+        let { order } = window.getComputedStyle(svg)
+        expect(order).to.eq('2')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+
+    {
+        // mock
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+                iconPosition: 'right',
+            }
+        })
+        vm.$mount()
+        let spy = chai.spy(function() {})
+        vm.$on('click', spy)
+        let button = vm.$el
+        button.click()
+        expect(spy).to.have.been.called()
+    }
+
+} catch (error) {
+    window.errors = [error]
+} finally {
+    window.errors && window.errors.forEach((error) => {
+        console.log(error.message)
     })
-    vm.$mount()
-    let spy = chai.spy(function(){})
-    vm.$on('click', spy)
-    let button = vm.$el
-    button.click()
-    expect(spy).to.have.been.called()
 }
--- src/button.vue
+++ src/button.vue
@@ -1,92 +1,94 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
-        <g-icon :name="icon" v-if="icon && !loading" class="icon"></g-icon>
-        <g-icon name="loading" v-if="loading" class="loading icon"></g-icon>
-        <div class="content">
-            <slot></slot>
-        </div>
-    </button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
+    <g-icon :name="icon" v-if="icon && !loading" class="icon"></g-icon>
+    <g-icon name="loading" v-if="loading" class="loading icon"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-    export default{
-//        props: ['icon', 'iconPosition'],
-        props: {
-            icon: {},
-            loading: {
-                type: Boolean,
-                default: false,
-            },
-            iconPosition: {
-                type: String,
-                default: 'left',
-                validator(value){
-//                    return !(value !== 'left' && value !== 'right');
-                    return value === 'left' || value === 'right'
-                },
-            }
-        }
+import Icon from "./icon.vue";
+export default {
+  components: {
+    "g-icon": Icon
+  },
+  //        props: ['icon', 'iconPosition'],
+  props: {
+    icon: {},
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        //                    return !(value !== 'left' && value !== 'right');
+        return value === "left" || value === "right";
+      }
     }
+  }
+};
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
-    @keyframes spin {
-        0% {
-            transform: rotate(0deg);
-        }
-        100% {
-            transform: rotate(360deg);
-        }
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  > .content {
+    order: 2;
+  }
+
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+
+  &.icon-right {
+    > .content {
+      order: 1;
     }
 
-    .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-        vertical-align: middle;
-
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-
-        &:active {
-            background-color: var(--button-active-bg);
-        }
-
-        &:focus {
-            outline: none;
-        }
-
-        > .content {
-            order: 2;
-        }
-
-        > .icon {
-            order: 1;
-            margin-right: .1em;
-        }
-
-        &.icon-right {
-
-            > .content {
-                order: 1;
-            }
-
-            > .icon {
-                order: 2;
-                margin-left: .1em;
-                margin-right: 0;
-            }
-
-        }
-        .loading {
-            animation: 2s spin infinite linear;
-        }
+    > .icon {
+      order: 2;
+      margin-left: 0.1em;
+      margin-right: 0;
     }
+  }
+  .loading {
+    animation: 2s spin infinite linear;
+  }
+}
 </style>
